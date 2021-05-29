8f3f042e9b174c73f970ed3733d171d2a9f8994f
vlad <caraseli02@gmail.com>
2021-05-29 15:55:44 +0200

    init github

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,35 @@
 module.exports = {
-  purge: ['./dist/**/*.html'],
+  purge: ["./dist/**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins"],
+      },
+      backgroundColor: {
+        primary: "var(--color-bg-primary)",
+        secondary: "var(--color-bg-secondary)",
+      },
+      textColor: {
+        accent: "var(--color-text-accent)",
+        primary: "var(--color-text-primary)",
+        secondary: "var(--color-text-secondary)",
+      },
+      backgroundImage: (theme) => ({
+        "hero-pattern": "url('../../assets/icons/Patterns.svg')",
+        "footer-texture": "url('/img/footer-texture.png')",
+      }),
+      backdropFilter: {
+        none: "none",
+        blur: "blur(20px)",
+      },
+      height: {
+        "50h": "50vh",
+      },
+      width: {
+        "50w": "50vw",
+      },
+    },
   },
   variants: {},
   plugins: [],
-}
+};
